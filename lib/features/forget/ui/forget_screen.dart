a3e7bc45431a3f6fb20df91ce30f176a7ff3426a
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-03-10 06:08:45 +0200

    add no internet screen

--- lib/features/forget/ui/forget_screen.dart
+++ lib/features/forget/ui/forget_screen.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 
 import '../../../core/widgets/already_have_account_text.dart';
+import '../../../core/widgets/no_internet.dart';
 import '../../../core/widgets/terms_and_conditions_text.dart';
 import '../../../themes/styles.dart';
 import 'widgets/password_reset.dart';
+
+class BuildForgetScreen extends StatelessWidget {
+  const BuildForgetScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding:
+            EdgeInsets.only(left: 30.w, right: 30.w, bottom: 15.h, top: 5.h),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Reset',
+                            style: TextStyles.font24White600Weight,
+                          ),
+                          Gap(10.h),
+                          Text(
+                            "Enter email to reset password",
+                            style: TextStyles.font14Grey400Weight,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gap(20.h),
+                    const PasswordReset(),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // Ensure minimum height
+                children: [
+                  const TermsAndConditionsText(),
+                  Gap(24.h),
+                  const AlreadyHaveAccountText(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class ForgetScreen extends StatelessWidget {
   const ForgetScreen({super.key});
@@ -13,52 +73,19 @@ class ForgetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding:
-              EdgeInsets.only(left: 30.w, right: 30.w, bottom: 15.h, top: 5.h),
-          child: Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Reset',
-                              style: TextStyles.font24White600Weight,
-                            ),
-                            Gap(10.h),
-                            Text(
-                              "Enter email to reset password",
-                              style: TextStyles.font14Grey400Weight,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Gap(20.h),
-                      const PasswordReset(),
-                    ],
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min, // Ensure minimum height
-                  children: [
-                    const TermsAndConditionsText(),
-                    Gap(24.h),
-                    const AlreadyHaveAccountText(),
-                  ],
-                ),
-              ),
-            ],
-          ),
+      body: OfflineBuilder(
+        connectivityBuilder: (
+          BuildContext context,
+          ConnectivityResult connectivity,
+          Widget child,
+        ) {
+          final bool connected = connectivity != ConnectivityResult.none;
+          return connected
+              ? const BuildForgetScreen()
+              : const BuildNoInternet();
+        },
+        child: const Center(
+          child: CircularProgressIndicator(),
         ),
       ),
     );
