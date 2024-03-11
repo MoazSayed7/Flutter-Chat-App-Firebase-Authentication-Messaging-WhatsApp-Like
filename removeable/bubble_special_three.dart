57e404602c82528a0dd0c4a40b689e3df8d0f15a
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-03-11 11:09:51 +0200

    replace your file with this

---
+++ removeable/bubble_special_three.dart
@@ -0,0 +1,264 @@
+import 'package:gap/gap.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+
+///iMessage's chat bubble type
+///
+///chat bubble color can be customized using [color]
+///chat bubble tail can be customized  using [tail]
+///chat bubble display message can be changed using [text]
+///[text] is the only required parameter
+///message sender can be changed using [isSender]
+///chat bubble [TextStyle] can be customized using [textStyle]
+
+class BubbleSpecialThree extends StatelessWidget {
+  final bool isSender;
+  final String text;
+  final String sendTime;
+  final bool tail;
+  final Color color;
+  final TextAlign textAlign;
+
+  const BubbleSpecialThree({
+    Key? key,
+    this.isSender = true,
+    required this.text,
+    required this.sendTime,
+    this.color = Colors.white70,
+    this.tail = true,
+    this.textAlign = TextAlign.left,
+  }) : super(key: key);
+
+  ///chat bubble builder method
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isSender ? Alignment.topRight : Alignment.topLeft,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        child: CustomPaint(
+          painter: SpecialChatBubbleThree(
+            color: color,
+            alignment: isSender ? Alignment.topRight : Alignment.topLeft,
+            tail: tail,
+          ),
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * .7,
+            ),
+            margin: isSender
+                ? const EdgeInsets.fromLTRB(7, 7, 17, 7)
+                : const EdgeInsets.fromLTRB(17, 7, 7, 7),
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 4, right: 4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        text,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.sp,
+                        ),
+                        textAlign: textAlign,
+                      ),
+                      Gap(3.h),
+                      Text(
+                        sendTime,
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+///custom painter use to create the shape of the chat bubble
+///
+/// [color],[alignment] and [tail] can be changed
+
+class SpecialChatBubbleThree extends CustomPainter {
+  final Color color;
+  final Alignment alignment;
+  final bool tail;
+
+  SpecialChatBubbleThree({
+    required this.color,
+    required this.alignment,
+    required this.tail,
+  });
+
+  final double _radius = 10.0;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var h = size.height;
+    var w = size.width;
+    if (alignment == Alignment.topRight) {
+      if (tail) {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 2, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(0, 0, 0, _radius * 1.5);
+
+        /// left line
+        path.lineTo(0, h - _radius * 1.5);
+
+        /// bottom-left corner
+        path.quadraticBezierTo(0, h, _radius * 2, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 3, h);
+
+        /// bottom-right bubble curve
+        path.quadraticBezierTo(
+            w - _radius * 1.5, h, w - _radius * 1.5, h - _radius * 0.6);
+
+        /// bottom-right tail curve 1
+        path.quadraticBezierTo(w - _radius * 1, h, w, h);
+
+        /// bottom-right tail curve 2
+        path.quadraticBezierTo(
+            w - _radius * 0.8, h, w - _radius, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w - _radius, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      } else {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 2, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(0, 0, 0, _radius * 1.5);
+
+        /// left line
+        path.lineTo(0, h - _radius * 1.5);
+
+        /// bottom-left corner
+        path.quadraticBezierTo(0, h, _radius * 2, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 3, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w - _radius, h, w - _radius, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w - _radius, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      }
+    } else {
+      if (tail) {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 3, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(_radius, 0, _radius, _radius * 1.5);
+
+        /// left line
+        path.lineTo(_radius, h - _radius * 1.5);
+        // bottom-right tail curve 1
+        path.quadraticBezierTo(_radius * .8, h, 0, h);
+
+        /// bottom-right tail curve 2
+        path.quadraticBezierTo(
+            _radius * 1, h, _radius * 1.5, h - _radius * 0.6);
+
+        /// bottom-left bubble curve
+        path.quadraticBezierTo(_radius * 1.5, h, _radius * 3, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 2, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w, h, w, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w, 0, w - _radius * 2, 0);
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      } else {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 3, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(_radius, 0, _radius, _radius * 1.5);
+
+        /// left line
+        path.lineTo(_radius, h - _radius * 1.5);
+
+        /// bottom-left curve
+        path.quadraticBezierTo(_radius, h, _radius * 3, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 2, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w, h, w, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w, 0, w - _radius * 2, 0);
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
