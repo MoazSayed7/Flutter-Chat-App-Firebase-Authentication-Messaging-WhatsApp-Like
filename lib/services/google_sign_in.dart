87b17fc35a7abdfa7091eabd4145b3f7489201b4
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-03-20 01:11:46 +0200

    Update isOnline from bool to String

--- lib/services/google_sign_in.dart
+++ lib/services/google_sign_in.dart
@@ -54,7 +54,7 @@ class GoogleSignin {
             'profilePic': _auth.currentUser!.photoURL,
             'uid': _auth.currentUser!.uid,
             'mtoken': await getToken(),
-            'isOnline': true,
+            'isOnline': 'true',
           },
           SetOptions(merge: true),
         );
