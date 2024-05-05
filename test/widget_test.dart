47d72dd263352b81c533fc9e7b9db23ad5c7b78e
Moaz Sayed <moazsaid7586@gmail.com>
2024-05-05 19:41:08 +0300

    - launch project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:chatapp/router/app_routes.dart';
+import 'package:chatchat/router/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chatapp/main.dart';
+import 'package:chatchat/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
