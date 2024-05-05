e0b6291f1b94338e754c679e6bdd6e0e23339a30
Moaz Sayed <moazsaid7586@gmail.com>
2024-05-05 18:59:11 +0300

    - Change App Name & Package Name - Add Splash Scree

---
+++ android/app/src/main/kotlin/com/chatchat/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chatchat.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
--- android/app/src/main/kotlin/com/example/chatchat/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chatchat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.chatapp
+package com.example.chatchat
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
